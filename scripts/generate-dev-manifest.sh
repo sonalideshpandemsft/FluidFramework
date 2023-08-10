96ff1185f4828ae5e32a6606a98169002d10266e
Sonali Deshpande <48232592+sonalideshpandemsft@users.noreply.github.com>
2023-08-10 19:04:21 +0000

    download caret file and update dev versions

--- scripts/generate-dev-manifest.sh
+++ scripts/generate-dev-manifest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo SETVERSION_VERSION=$SETVERSION_VERSION 
+echo SETVERSION_VERSION=$SETVERSION_VERSION
+
+# Set the version environment variable
+export SETVERSION_VERSION=$SETVERSION_VERSION
 
 # Fetch releases using GitHub API
 releases=$(curl -s "https://api.github.com/repos/microsoft/FluidFramework/releases")
@@ -11,21 +14,19 @@ manifest_url_caret=$(echo "$releases" | jq -r '.[0] | .assets[] | select(.name |
 if [ -n "$manifest_url_caret" ]; then
   echo "Downloading latest internal manifest: $manifest_url_caret"
   curl -LOJ "$manifest_url_caret"
-fi
-
-# Find the latest simple.json internal manifest file URL
-manifest_url_simple=$(echo "$releases" | jq -r '.[0] | .assets[] | select(.name | contains(".simple.json")) | .browser_download_url')
-
-if [ -n "$manifest_url_simple" ]; then
-  echo "Downloading latest internal manifest: $manifest_url_simple"
-  curl -LOJ "$manifest_url_simple"
 
   # Extract the filename from the URL
-  manifest_filename=$(basename "$manifest_url_simple")
+  manifest_filename=$(basename "$manifest_url_caret")
 
-  # Modify the downloaded manifest file using jq
-  jq 'to_entries | map(if .value | contains("internal") then . + {value: $SETVERSION_VERSION} else . end) | from_entries' "$manifest_filename" > temp.json && mv temp.json "$manifest_filename"
-  
+  # New file name with version variable
+  new_manifest_filename="fluid-framework-release-manifest.client.${SETVERSION_VERSION}.caret.json"
+
+  # Rename the downloaded manifest file
+  mv "$manifest_filename" "$new_manifest_filename"
+
+  # Modify the downloaded manifest file using jq with version variable
+  jq --arg version "$SETVERSION_VERSION" 'to_entries | map(if .value | contains("internal") then . + {value: $version} else . end) | from_entries' "$new_manifest_filename" > temp.json && mv temp.json "$new_manifest_filename"
+
   echo "Manifest modified successfully."
 
   # Upload simple.json and caret.json manifest files to azure blob
